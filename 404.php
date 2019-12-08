c3d8bda94b0d4df80afc7fd31e739834b988db28
Tomas Gajardo <tooomas821@gmail.com>
2019-12-07 22:12:57 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@ get_header();?>
         <div class="col-md-6 text-center">
           <span class="display-1 d-block">404</span>
            <div class="mb-4 lead">The page you are looking for was not found.</div>
-         <a href="https://www.totoprayogo.com/#" class="text-light">Back to Home</a>
+         <a href="<?php echo get_home_url(); ?>" class="text-light">Back to Home</a>
        </div>
       </div>
     </div>
