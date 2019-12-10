5e84bb2c00c66d4da43f63216dd6f989c9bf03d3
DarkNadie <darknadie@outlook.com>
2019-12-10 02:09:48 -0300

    formulario de contacto actualizado

--- contacto.php
+++ contacto.php
@@ -34,9 +34,9 @@ get_header();?>
                   <div class="form-group col-md-6">
                     <input type="text" name="name" class="form-control" required="required"
                      tavindex="1" accesskey="N" title="Nombre:" placeholder="Su Nombre" autofocus>
-                    <input type="text" name="company" class="form-control" tavindex="2" accesskey="C" title="Compañía" placeholder="El Nombre De Su Compañia"> </div>
+                    <input type="text" name="company" class="form-control" tavindex="2" accesskey="C" title="Compañía:" placeholder="El Nombre De Su Compañia"> </div>
                   <div class="form-group col-md-6">
-                    <input type="email" name="email" class="form-control" required="required" placeholder="Direccion Email">
+                    <input type="email" name="email" class="form-control" required="required" tavindex="3" accesskey="E" title="E-Mail:" placeholder="Su Direccion Email">
                     <input type="url" name="website" class="form-control" placeholder="Sitio Web"> </div>
                   <div class="form-group col-md-12"> <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Mensaje"></textarea> </div>
                 </div>
