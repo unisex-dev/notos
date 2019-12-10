5c7a60010f979ee8bfc3fd59c75e20b5c9d7136d
Tomas Gajardo <tooomas821@gmail.com>
2019-12-10 03:52:38 -0300

    Formulario separado incluido en contacto

--- formulario.php
+++ formulario.php
@@ -8,6 +8,7 @@
                     <input type="url" name="website" class="form-control" placeholder="Sitio Web"> </div>
                   <div class="form-group col-md-12"> <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Mensaje"></textarea> </div>
                 </div>
+                
                 <div class="form-group">
                   <button type="submit" class="btn btn-block btn-dark">Enviar</button>
                 </div>
