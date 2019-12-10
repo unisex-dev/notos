5f276882b6945ffb82f02dd8938c545bdd0e15b8
DarkNadie <darknadie@outlook.com>
2019-12-10 02:14:33 -0300

    formulario buscar actualizado

--- searchform.php
+++ searchform.php
@@ -6,7 +6,7 @@
     <div class="mb-5">
     <form id="buscar" name="buscar" role="search" method="get" class="form search-form" action="/index.php">
     <div class="input-group">
-	<input class="form-control" type="search" name="buscar" placeholder="<?php echo esc_attr( 'Buscar…', 'presentation' ); ?>" tabindex="1" accesskey="E" title="buscar:" autofocus name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>">
+	<input class="form-control" type="search" name="buscar" placeholder="<?php echo esc_attr( 'Buscar…', 'presentation' ); ?>" tabindex="1" accesskey="E" title="buscar:" autofocus     name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>">
 
     <span class="input-group-btn">
 	<button class="btn btn-secondary"  id="search-submit" type="submit" accesskey="B" tabindex="2" title="buscar"><i class="fa fa-search"></i></button>
