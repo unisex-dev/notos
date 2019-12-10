fe46f7d362bb79c795d362f8a3729b459c4f701a
Tomas Gajardo <tooomas821@gmail.com>
2019-12-10 03:50:23 -0300

    1.2.4

--- contacto.php
+++ contacto.php
@@ -29,20 +29,7 @@ get_header();?>
         <div class="p-0 col-md-8">
           <div class="card bg-dark rounded-0">
             <div class="card-body p-5">
-              <form id="contact-form" class="contact-form" name="contact-form" method="post" action="#">
-                <div class="row">
-                  <div class="form-group col-md-6">
-                    <input type="text" name="name" class="form-control" required="required" placeholder="Nombre">
-                    <input type="text" name="company" class="form-control" placeholder="Compañia"> </div>
-                  <div class="form-group col-md-6">
-                    <input type="email" name="email" class="form-control" required="required" placeholder="Direccion Email">
-                    <input type="url" name="website" class="form-control" placeholder="Sitio Web"> </div>
-                  <div class="form-group col-md-12"> <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Mensaje"></textarea> </div>
-                </div>
-                <div class="form-group">
-                  <button type="submit" class="btn btn-block btn-dark">Enviar</button>
-                </div>
-              </form>
+            <?php get_template_part( 'formulario', 'page' ); ?>
             </div>
           </div>
         </div>
