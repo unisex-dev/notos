d0944a3d56f2fd89c8143e0073e226e257b471c9
DarkNadie <darknadie@outlook.com>
2019-12-10 02:03:27 -0300

    formulario de contacto

--- contacto.php
+++ contacto.php
@@ -32,8 +32,9 @@ get_header();?>
               <form id="contact-form" class="contact-form" name="contact-form" method="post" action="#">
                 <div class="row">
                   <div class="form-group col-md-6">
-                    <input type="text" name="name" class="form-control" required="required" placeholder="Nombre">
-                    <input type="text" name="company" class="form-control" placeholder="Compañia"> </div>
+                    <input type="text" name="name" class="form-control" required="required"
+                     tavindex="1" accesskey="N" title="Nombre:" placeholder="Su Nombre" autofocus>
+                    <input type="text" name="company" class="form-control" tavindex="2" accesskey="C" title="Compañía" placeholder="El Nombre De Su Compañia"> </div>
                   <div class="form-group col-md-6">
                     <input type="email" name="email" class="form-control" required="required" placeholder="Direccion Email">
                     <input type="url" name="website" class="form-control" placeholder="Sitio Web"> </div>
