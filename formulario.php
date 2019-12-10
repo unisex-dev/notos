9fc78dbd7e2ca887f99c1b396bae55dfd0406466
Tomas Gajardo <tooomas821@gmail.com>
2019-12-10 19:36:52 -0300

    se traslaron cambios de marcelo a este formulario

--- formulario.php
+++ formulario.php
@@ -1,4 +1,5 @@
               <form id="contact-form" class="contact-form" name="contact-form" method="post" action="#">
+                
                 <div class="row">
                   <div class="form-group col-md-6">
                     <input type="text" name="name" class="form-control" required="required"
@@ -9,7 +10,9 @@
                     <input type="url" name="website" class="form-control" placeholder="Sitio Web"> </div>
                   <div class="form-group col-md-12"> <textarea name="message" id="message" required="required" class="  form-control" rows="8" placeholder="Mensaje"></textarea> </div>
                 </div>
+                
                 <div class="form-group">
                   <button type="submit" class="btn btn-block btn-dark">Enviar</button>
                 </div>
+                
               </form>
